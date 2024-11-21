5ffe0785ee072ba4fb2fcf8ed2fefe2d12c4c5c5
DuckyDollars <duckydollars5@gmal.com>
2024-11-21 16:51:26 +0300

    las commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Header from '@/components/Header'
 import TabContainer from '@/components/HomeTab'
 import NavigationBar from '@/components/NavigationBar'
 
 export default function Home() {
   return (
       <main className="min-h-screen bg-black text-white">
-        <Header />
         <TabContainer />
         <NavigationBar />
       </main>
