9362e3edf279ddd5bb29a2ff38582a50abafb079
DuckyDollars <duckydollars5@gmal.com>
2024-11-21 17:58:23 +0300

    8080

---
+++ app/friends/page.tsx
@@ -0,0 +1,11 @@
+import TabContainer from '@/components/Friends'
+import NavigationBar from '@/components/NavigationBar'
+
+export default function Home() {
+  return (
+      <main className="min-h-screen bg-black text-white">
+        <TabContainer />
+        <NavigationBar />
+      </main>
+  )
+}
