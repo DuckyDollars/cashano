3592a086349da57d857fcd0aefb4233b8a87ecae
DuckyDollars <duckydollars5@gmal.com>
2024-11-28 17:47:02 +0300

    zer

---
+++ utils/types.ts
@@ -0,0 +1,6 @@
+export type IconProps = {
+    size?: number;
+    className?: string;
+}
+
+export type TabType = 'home' | 'profile' | 'invest'| 'wallet'| 'friends' | 'stock' | 'golden' | 'crypto' | 'diamond';
