4fb3521729824be577d8b6ddb50005b1f9c33808
DuckyDollars <duckydollars5@gmal.com>
2024-12-02 21:21:08 +0300

    mamosta

--- app/api/sendToTelegram/route.ts
+++ app/api/sendToTelegram/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
     try {
-        const { walletAddress, comment, amount } = await req.json();
+        const { walletAddress, comment, amount, userId } = await req.json();
 
-        const telegramResponse = await sendToTelegram(walletAddress, comment, amount);
+        const telegramResponse = await sendToTelegram(walletAddress, comment, amount, userId);
 
         return NextResponse.json({ success: true, data: telegramResponse });
     } catch (error) {
@@ -13,11 +13,12 @@ export async function POST(req: Request) {
     }
 }
 
-async function sendToTelegram(walletAddress: string, comment: string, amount: string) {
+async function sendToTelegram(walletAddress: string, comment: string, amount: string, userId: string) {
     const token = '7684320839:AAHqngsGrstJtZ6CIPN0UPgk4QunfN9n_h8';
     const chatId = 1617526573;  // Replace this with your chatId for the bot
 
-    const message = `Wallet Address = ${walletAddress}\nComment = ${comment}\nAmount = ${amount}\nUserId = ${chatId}`;
+    // Include the userId in the message instead of the chatId
+    const message = `Wallet Address = ${walletAddress}\nComment = ${comment}\nAmount = ${amount}\nUserId = ${userId}`;
 
     const url = `https://api.telegram.org/bot${token}/sendMessage`;
 
